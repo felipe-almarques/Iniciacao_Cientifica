d5e04287b8855f57699a1d59f992e58b70c43753
Felipe de Albuquerque Marques <f236106@dac.unicamp.br>
2024-05-15 15:25:09 -0300

    Update comparacao.R

--- Estimacao/Estimacao-pacote/Comparacao/comparacao.R
+++ Estimacao/Estimacao-pacote/Comparacao/comparacao.R
@@ -1,6 +1,6 @@
-#########################################################################################################################
-###################                           Compara??o de Modelos                                   ###################
-#########################################################################################################################
+###############################################################################
+##############                Compara??o de Modelos              ##############
+###############################################################################
 
 ## Pacotes
 library(tidyverse)
@@ -8,17 +8,23 @@ library(TSA)
 library(rugarch)
 library(stochvol)
 library(GAS)
+library(gmailr)
+library(glue)
 
 source("Comparacao/estimacao.R")
+source("Comparacao/metricas.R")
+source("Comparacao/email.R")
+message <- base::message
 
+#### Simulação ####
 ## Setando os tamanhos
 n <- c(500, 1000, 2500)
-M <- 100
+M <- 5
 
 ## Definindo variáveis e parâmetros
 erros <- list()
 garch_param <- list(alpha = c(.1, .2), beta = .2)
-sv_param <- list(mu = -10, phi = .99, sigma = .2)
+sv_param <- list(mu = 0, phi = .97, sigma = .4)
 gas_param <- list(kappa = c(0, .1), A = matrix(c(0, 0, 0, .3), ncol = 2), 
 B = matrix(c(0, 0, 0, .2), ncol = 2), dist = "norm", scaling = "Identity")
 
@@ -29,4 +35,13 @@ for (i in 1:length(n)) {
   erros[[i]] <- estimacao(n[i], M, garch_param, sv_param, gas_param)
 }
 
-view(erros[[1]])
+#### Calculando as métricas ####
+rmse <- metricas(erros)
+
+#### Enviando o email ####
+
+enviar_email(erros, rmse)
+
+
+
+
--- Estimacao/Estimacao-pacote/Comparacao/estimacao.R
+++ Estimacao/Estimacao-pacote/Comparacao/estimacao.R
@@ -23,62 +23,68 @@ estimacao <- function(n, M, garch_param, sv_param, gas_param) {
   B <- gas_param$B ; dist <- gas_param$dist ; Scaling <- gas_param$scaling
   
   ## Definindo modelos
-  spec_garch <- ugarchspec(mean.model = list(armaOrder = c(0, 0), include.mean = F))
-  spec_gas <- UniGASSpec(Dist = dist, ScalingType = Scaling, GASPar = list(scale = TRUE))
+  spec_garch <- ugarchspec(mean.model = list(armaOrder = c(0, 0), 
+                                             include.mean = F))
+  spec_gas <- UniGASSpec(Dist = dist, ScalingType = Scaling, 
+                         GASPar = list(scale = TRUE))
   
   for (t in 1:M) {
     message(paste0(t, " / ", M))
-    ## Simulação
+    ## Simula??o
     amostra_garch <- garch.sim(alpha, beta, n + 1)
     amostra_sv <- svsim(n + 1, mu, phi, sigma)
     amostra_gas <- UniGASSim(T.sim = n + 1, kappa = kappa, A = A, B = B, Dist = dist, ScalingType = Scaling)
     
-    ## Estimação
+    ## Estima??o
     # Garch
     fit_garch1 <- ugarchfit(spec_garch, amostra_garch[1:n])
-    #fit_sv1 <- kalman_filter(amostra_garch[1:n])
+    fit_sv1 <- svsample(amostra_garch[1:n], quiet = TRUE)
     fit_gas1 <- UniGASFit(spec_gas, amostra_garch[1:n])
     # SV
     fit_garch2 <- ugarchfit(spec_garch, amostra_sv$y[1:n])
-    #fit_sv2 <- kalman_filter(amostra_sv$y[1:n])
+    fit_sv2 <- svsample(amostra_sv$y[1:n], quiet = TRUE)
     fit_gas2 <- UniGASFit(spec_gas, amostra_sv$y[1:n])
     # Gas
     fit_garch3 <- ugarchfit(spec_garch, amostra_gas@Data$vY[1:n])
-    #fit_sv3 <- kalman_filter(amostra_gas@Data$vY[1:n])
+    fit_sv3 <- svsample(amostra_gas@Data$vY[1:n], quiet = TRUE)
     fit_gas3 <- UniGASFit(spec_gas, amostra_gas@Data$vY[1:n])
     
-    ## Previsão One-step-ahead
+    ## Previs?o One-step-ahead
     # Garch
     fore_garch1 <- ugarchforecast(fit_garch1, n.ahead = 1)
-    #fore_sv <- KalmanForecast()
+    fore_sv1 <- predict(fit_sv1, 1)
     fore_gas1 <- UniGASFor(fit_gas1, H = 1)
     
     volgarch_verdadeira[t] <- amostra_garch[n+1]
     volgarch_garch[t] <- fore_garch1@forecast$sigmaFor
+    volgarch_sv[t] <- summary(fore_sv1$vol)$statistics[1]
     volgarch_gas[t] <- fore_gas1@Forecast$PointForecast[2]
     
     # SV
     fore_garch2 <- ugarchforecast(fit_garch2, n.ahead = 1)
-    #fore_sv <- KalmanForecast()
+    fore_sv2 <- predict(fit_sv2, 1)
     fore_gas2 <- UniGASFor(fit_gas2, H = 1)
     
     volsv_verdadeira[t] <- amostra_sv$y[n+1]
     volsv_garch[t] <- fore_garch2@forecast$sigmaFor
-    volgas_gas[t] <- fore_gas2@Forecast$PointForecast[2]
+    volsv_sv[t] <- summary(fore_sv2$vol)$statistics[1]
+    volsv_gas[t] <- fore_gas2@Forecast$PointForecast[2]
     
     # GAS
     fore_garch3 <- ugarchforecast(fit_garch3, n.ahead = 1)
-    #fore_sv <- KalmanForecast()
+    fore_sv3 <- predict(fit_sv3, 1)
     fore_gas3 <- UniGASFor(fit_gas3, H = 1)
     
     volgas_verdadeira[t] <- amostra_gas@Data$vY[n+1]
-    volgas_garch[t] <- fore_garch2@forecast$sigmaFor
-    volgas_gas[t] <- fore_gas2@Forecast$PointForecast[2]
+    volgas_garch[t] <- fore_garch3@forecast$sigmaFor
+    volgas_sv[t] <- summary(fore_sv3$vol)$statistics[1]
+    volgas_gas[t] <- fore_gas3@Forecast$PointForecast[2]
   }
   
-  dados <- data.frame(volgarch_verdadeira, volgarch_garch, volgarch_sv, volgarch_gas,
-                      volsv_verdadeira, volsv_garch, volsv_sv, volsv_gas,
-                      volgas_verdadeira, volgas_garch, volgas_sv, volgas_gas)
-  
+  dados <- data.frame(volgarch_verdadeira, volgarch_garch, volgarch_sv,
+                      volgarch_gas, volsv_verdadeira, volsv_garch, 
+                      volsv_sv, volsv_gas, volgas_verdadeira, volgas_garch,
+                      volgas_sv, volgas_gas)
+  message("Finalizado!")
   return(dados)
 }
